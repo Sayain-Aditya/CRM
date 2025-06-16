31dbadae969fd045cb99e47b6c07981e478cf4a5
Sayain-Aditya <as4316664@gmail.com>
2025-06-16 11:07:45 +0530

    logon

--- src/App.jsx
+++ src/App.jsx
@@ -170,7 +170,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<Navigate to="/Dashboard" replace />} />
         </Routes>
       )}
     </>
